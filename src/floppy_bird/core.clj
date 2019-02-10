9a16c7fb16888348347f86aec5d98f013454249f
Robin Schroer <sulami@peerwire.org>
2019-02-10 22:16:19 +0100

    draw a bird and some pipes

--- src/floppy_bird/core.clj
+++ src/floppy_bird/core.clj
@@ -5,15 +5,23 @@
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :rgb)
-  {})
+  {:bird-height 250
+   :pipes '([300 150 250])})
 
 (defn update-state [state]
   state)
 
 (defn draw-state [state]
   (q/background 240)
-  (q/fill (:color state) 255 255))
+  (q/fill 255)
 
+  (q/rect-mode :center)
+  (q/rect 30 (:bird-height state) 10 10)
+
+  (q/rect-mode :corner)
+  (doseq [[x top bottom] (:pipes state)]
+    (q/rect x 0 30 top)
+    (q/rect x bottom 30 (- 500 bottom))))
 
 (q/defsketch floppy-bird
   :title "Floppy bird"
@@ -23,3 +31,5 @@
   :draw draw-state
   :features [:keep-on-top]
   :middleware [m/fun-mode])
+
+(defn -main [])
